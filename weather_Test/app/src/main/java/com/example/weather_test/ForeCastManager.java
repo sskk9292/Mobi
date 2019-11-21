2e048c194d4862ab10b65a0a0e59a9e38856903c
seokgiLee <plot_lee@naver.com>
2019-11-22 02:41:43 +0900

    날씨어플 테스트 ver.1
    현재날씨 표시

---
+++ weather_Test/app/src/main/java/com/example/weather_test/ForeCastManager.java
@@ -0,0 +1,111 @@
+package com.example.weather_test;
+
+import android.content.ContentValues;
+import android.os.StrictMode;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class ForeCastManager extends Thread{
+    String lon,lat;
+
+    ArrayList<ContentValues> mWeatehr;
+    MainActivity mContext;
+    public ArrayList<ContentValues> getmWeather()
+    {
+        return mWeatehr;
+    }
+
+    public ForeCastManager(String lon, String lat,MainActivity mContext)
+    {
+        this.lon = lon ; this.lat = lat;
+        this.mContext = mContext;
+    }
+
+
+
+    public ArrayList<ContentValues> GetOpenWeather(String lon,String lat)
+    {
+
+        ArrayList<ContentValues> mTotalValue = new ArrayList<ContentValues>();
+        String key = "137d2ee14146fa9d3bef3098beb86df3";
+        try{
+            URL url = new URL("https://api.openweathermap.org/data/2.5/weather/?"+
+                    "&lat="+lat+
+                    "&lon="+lon+
+                    "&mode=xml" +
+                    "&units=metric"+
+                    "&appid="+key);
+
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            // 위에서 생성된 URL을 통하여 서버에 요청하면 결과가 XML Resource로 전달됨
+            XmlPullParser parser = factory.newPullParser();
+            // XML Resource를 파싱할 parser를 factory로 생성
+            parser.setInput(url.openStream(), null);
+            // 파서를 통하여 각 요소들의 이벤트성 처리를 반복수행
+            int parserEvent = parser.getEventType();
+            while (parserEvent != XmlPullParser.END_DOCUMENT) {
+                if(parserEvent == XmlPullParser.START_TAG  && parser.getName().equals("temperature")){
+                    //시작태그의 이름을 알아냄
+                    int checkStartTag = parserEvent;
+                    ContentValues mContent = new ContentValues();
+
+                    for( ; ; ) {
+                        if (checkStartTag == XmlPullParser.START_TAG  && parser.getName().equals("temperature")) {
+                            mContent.put("temp_Now", parser.getAttributeValue(null, "value"));
+                            mContent.put("temp_Min", parser.getAttributeValue(null, "min"));
+                            mContent.put("temp_Max", parser.getAttributeValue(null, "max"));
+                        } else if (checkStartTag == XmlPullParser.START_TAG  && parser.getName().equals("humidity")) {
+                            mContent.put("humidity", parser.getAttributeValue(null, "value"));
+                        } else if (checkStartTag == XmlPullParser.START_TAG  && parser.getName().equals("speed")) {
+                            mContent.put("wind_Speed", parser.getAttributeValue(null, "value"));
+                            mContent.put("wind_Name", parser.getAttributeValue(null, "name"));
+                        } else if (checkStartTag == XmlPullParser.START_TAG  && parser.getName().equals("direction")) {
+                            mContent.put("wind_Direction", parser.getAttributeValue(null, "name"));
+                            mContent.put("wind_SortNumber", parser.getAttributeValue(null, "value"));
+                            mContent.put("wind_SortCode", parser.getAttributeValue(null, "code"));
+                        } else if (checkStartTag == XmlPullParser.START_TAG  && parser.getName().equals("clouds")) {
+                            mContent.put("cloud_Amount", parser.getAttributeValue(null, "value"));
+                            mContent.put("cloud_Name", parser.getAttributeValue(null, "name"));
+                        } else if (checkStartTag == XmlPullParser.START_TAG  && parser.getName().equals("weather")) {
+                            mContent.put("weather_Sort", parser.getAttributeValue(null, "number"));
+                            mContent.put("weather_Name", parser.getAttributeValue(null, "value"));
+                            mTotalValue.add(mContent);
+                            break;
+                        }
+                        checkStartTag = parser.next();
+                    }
+
+                }
+                parserEvent = parser.next();
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return mTotalValue;
+    }
+
+
+
+    @Override    public void run() {
+        super.run();
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+        mWeatehr = GetOpenWeather(lon,lat);
+        mContext.handler.sendEmptyMessage(mContext.THREAD_HANDLER_SUCCESS_INFO);
+        //Thread 작업 종료, UI 작업을 위해 MainHandler에 Message보냄
+    }
+}
---
+++ weather_Test/app/src/main/java/com/example/weather_test/WeatherInfo.java
@@ -0,0 +1,99 @@
+package com.example.weather_test;
+
+public class WeatherInfo {
+    String wind_Direction;
+    String wind_SortNumber;
+    String wind_SortCode;
+    String wind_Speed;
+    String wind_Name;
+    String temp_Min;
+    String temp_Max;
+    String temp_Now;
+    String humidity;
+    String cloud_Amount;
+    String cloud_Name;
+    String weather_Sort;
+    String weather_Name;
+
+    public WeatherInfo(String temp_Now,
+                       String temp_Min,
+                       String temp_Max,
+                       String humidity,
+                       String wind_Speed,
+                       String wind_Name,
+                       String wind_Direction,
+                       String wind_SortNumber,
+                       String wind_SortCode,
+                       String cloud_Amount,
+                       String cloud_Name,
+                       String weather_Sort,
+                       String weather_Name)
+    {
+        this.wind_Direction = wind_Direction;
+        this.wind_SortNumber = wind_SortNumber;
+        this.wind_SortCode = wind_SortCode;
+        this.wind_Speed = wind_Speed;
+
+        if(wind_Name.equals("")) this.wind_Name = "No Info";
+        else this.wind_Name = wind_Name;
+
+        this.temp_Min = temp_Min;
+        this.temp_Max = temp_Max;
+        this.temp_Now = temp_Now;
+        this.humidity = humidity;
+
+        this.cloud_Amount = cloud_Amount;
+        this.cloud_Name = cloud_Name;
+
+        this.weather_Sort = weather_Sort;
+        this.weather_Name = weather_Name;
+    }
+
+    public String getWind_Speed() {
+        return wind_Speed;
+    }
+
+    public String getWind_Name() {
+        return wind_Name;
+    }
+
+    public String getTemp_Min() {
+        return temp_Min;
+    }
+
+    public String getTemp_Max() {
+        return temp_Max;
+    }
+
+    public String getTemp_Now() {
+        return temp_Now;
+    }
+
+    public String getHumidity() {
+        return humidity;
+    }
+
+    public String getCloud_Amount() {
+        return cloud_Amount;
+    }
+
+    public String getCloud_Name() {
+        return cloud_Name;
+    }
+
+    public String getWeather_Name() {
+        return weather_Name;
+    }
+
+    public void setWind_Name(String wind_Name){
+        this.wind_Name = wind_Name;
+    }
+
+    public void setCloud_Name(String cloud_Name){
+        this.cloud_Name = cloud_Name;
+    }
+
+    public void setWeather_Name(String weather_Name){
+        this.weather_Name = weather_Name;
+    }
+}
